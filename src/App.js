06741d0bc125a2ad97e4d959e1c1c814e6039215
Lunairefine <teamcyber2809@gmail.com>
2023-10-26 10:10:46 +0700

    Kopi kopi

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 class="headline">FERSTAWN.</h1>
+        <div className="container">
+          <p id="autoType">それでも, 僕だけは忘れない 物語</p>
+        </div>
+        <div className="buttonNext">
+          <a href="#">
+            <button>Explore</button>
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
