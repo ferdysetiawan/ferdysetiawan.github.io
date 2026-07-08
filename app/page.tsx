4a1982f63d601bb7a18b2f45209fdb975e1fd80f
ferdysetiawan <ferdysetiawan09@gmail.com>
2026-07-08 23:27:41 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Header from '@/app/components/header';
 import Landing from '@/app/components/landing';
 import Canvas from '@/app/components/canvas';
 import Footer from '@/app/components/footer';
+import BackgroundAudio from '@/app/components/background-audio';
 
 export default function Home() {
   return (
     <>
-      <audio src="/media/music.wav" autoPlay loop playsInline style={{ display: "none" }} />
+      <BackgroundAudio />
       <Header/>
       <Landing/>
       <Canvas/>
